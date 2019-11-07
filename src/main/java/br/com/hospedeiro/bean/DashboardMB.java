4a3ce108ebaccb1e85c4fdd8d0a852805ce6db17
William Bigas Mauro <williambmauro@hotmail.com>
2019-11-07 14:20:47 -0300

    Corrigido cores do dashboard

--- src/main/java/br/com/hospedeiro/bean/DashboardMB.java
+++ src/main/java/br/com/hospedeiro/bean/DashboardMB.java
@@ -47,6 +47,12 @@ public class DashboardMB implements Serializable {
         bgColors.add("rgb(255, 99, 132)");
         bgColors.add("rgb(54, 162, 235)");
         bgColors.add("rgb(255, 205, 86)");
+        bgColors.add("rgb(80, 60, 86)");
+        bgColors.add("rgb(182, 205, 86)");
+        bgColors.add("rgb(140, 205, 86)");
+        bgColors.add("rgb(160, 205, 86)");
+        bgColors.add("rgb(120, 205, 86)");
+        bgColors.add("rgb(100, 205, 86)");
         dataSet.setBackgroundColor(bgColors);
 
         data.addChartDataSet(dataSet);
@@ -66,5 +72,14 @@ public class DashboardMB implements Serializable {
 
     public void setPieModel(PieChartModel pieModel) {
         this.pieModel = pieModel;
+    }
+
+    public static Integer numRandom(Integer numInicial, Integer numFinal) {
+        java.util.Random gerador = new java.util.Random();
+        for (int i = 0; i < 1; i++) { //Sequencia da mega sena
+            Integer numeroR = gerador.nextInt((numFinal + 1) - numInicial) + numInicial;
+            return numeroR;
+        }
+        return null;
     }
 }
