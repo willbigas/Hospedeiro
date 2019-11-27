60a13ed68faa76afa56eb06c660f1e7973b05c39
William Bigas Mauro <williambmauro@hotmail.com>
2019-11-27 10:18:04 -0300

    Corrigido função de internacionalização que estava resetando ao trocar de página

--- src/main/java/br/com/hospedeiro/bean/IdiomaMB.java
+++ src/main/java/br/com/hospedeiro/bean/IdiomaMB.java
@@ -1,5 +1,6 @@
 package br.com.hospedeiro.bean;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -11,9 +12,28 @@ import java.util.Locale;
 public class IdiomaMB implements Serializable {
 
     private String idiomaSelecionado;
+    private Locale locale;
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
 
     public void setIdiomaSelecionado(String idiomaSelecionado) {
+        locale = new Locale(idiomaSelecionado);
         FacesContext.getCurrentInstance()
-                .getViewRoot().setLocale(new Locale(idiomaSelecionado));
+                .getViewRoot().setLocale(locale);
+    }
+
+    public String getIdiomaSelecionado() {
+        return idiomaSelecionado;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
     }
 }
